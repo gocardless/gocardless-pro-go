4583ba9a262aa4a64e8a62ff8e5ad635b3c4a8db
Robot <robot@gocardless.com>
2022-04-04 08:26:40 +0000

    Changes generated by 9e4cc2d3e9bc8d4b9d0f9183377407ac66630992

--- institution_service.go
+++ institution_service.go
@@ -25,12 +25,11 @@ type InstitutionServiceImpl struct {
 
 // Institution model
 type Institution struct {
-	CountryCode string   `url:"country_code,omitempty" json:"country_code,omitempty"`
-	IconUrl     string   `url:"icon_url,omitempty" json:"icon_url,omitempty"`
-	Id          string   `url:"id,omitempty" json:"id,omitempty"`
-	LogoUrl     string   `url:"logo_url,omitempty" json:"logo_url,omitempty"`
-	Name        string   `url:"name,omitempty" json:"name,omitempty"`
-	Roles       []string `url:"roles,omitempty" json:"roles,omitempty"`
+	CountryCode string `url:"country_code,omitempty" json:"country_code,omitempty"`
+	IconUrl     string `url:"icon_url,omitempty" json:"icon_url,omitempty"`
+	Id          string `url:"id,omitempty" json:"id,omitempty"`
+	LogoUrl     string `url:"logo_url,omitempty" json:"logo_url,omitempty"`
+	Name        string `url:"name,omitempty" json:"name,omitempty"`
 }
 
 type InstitutionService interface {
