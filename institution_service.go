a53a831a1709cc55544564d08d07594ade688eaf
Robot <robot@gocardless.com>
2022-06-06 12:51:27 +0000

    Changes generated by 27986531d1181190d301226dc2199d4e03fe3830

--- institution_service.go
+++ institution_service.go
@@ -25,11 +25,12 @@ type InstitutionServiceImpl struct {
 
 // Institution model
 type Institution struct {
-	CountryCode string `url:"country_code,omitempty" json:"country_code,omitempty"`
-	IconUrl     string `url:"icon_url,omitempty" json:"icon_url,omitempty"`
-	Id          string `url:"id,omitempty" json:"id,omitempty"`
-	LogoUrl     string `url:"logo_url,omitempty" json:"logo_url,omitempty"`
-	Name        string `url:"name,omitempty" json:"name,omitempty"`
+	BankRedirect bool   `url:"bank_redirect,omitempty" json:"bank_redirect,omitempty"`
+	CountryCode  string `url:"country_code,omitempty" json:"country_code,omitempty"`
+	IconUrl      string `url:"icon_url,omitempty" json:"icon_url,omitempty"`
+	Id           string `url:"id,omitempty" json:"id,omitempty"`
+	LogoUrl      string `url:"logo_url,omitempty" json:"logo_url,omitempty"`
+	Name         string `url:"name,omitempty" json:"name,omitempty"`
 }
 
 type InstitutionService interface {
