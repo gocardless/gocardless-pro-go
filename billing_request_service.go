7c132cf5a267d3c7426d533b092cc438b9f69c06
Robot <robot@gocardless.com>
2022-09-28 12:46:53 +0000

    Changes generated by c21a2be2886cbbfec8a96f2cb8f84eb22ec3a4f9

--- billing_request_service.go
+++ billing_request_service.go
@@ -61,16 +61,27 @@ type BillingRequestLinks struct {
 	PaymentRequestPayment string `url:"payment_request_payment,omitempty" json:"payment_request_payment,omitempty"`
 }
 
+type BillingRequestMandateRequestConsentParameters struct {
+	EndDate              string `url:"end_date,omitempty" json:"end_date,omitempty"`
+	MandatePurposeCode   string `url:"mandate_purpose_code,omitempty" json:"mandate_purpose_code,omitempty"`
+	MaxAmountPerPayment  int    `url:"max_amount_per_payment,omitempty" json:"max_amount_per_payment,omitempty"`
+	MaxAmountPerPeriod   int    `url:"max_amount_per_period,omitempty" json:"max_amount_per_period,omitempty"`
+	MaxPaymentsPerPeriod int    `url:"max_payments_per_period,omitempty" json:"max_payments_per_period,omitempty"`
+	Period               string `url:"period,omitempty" json:"period,omitempty"`
+	StartDate            string `url:"start_date,omitempty" json:"start_date,omitempty"`
+}
+
 type BillingRequestMandateRequestLinks struct {
 	Mandate string `url:"mandate,omitempty" json:"mandate,omitempty"`
 }
 
 type BillingRequestMandateRequest struct {
-	Currency string                             `url:"currency,omitempty" json:"currency,omitempty"`
-	Links    *BillingRequestMandateRequestLinks `url:"links,omitempty" json:"links,omitempty"`
-	Metadata map[string]interface{}             `url:"metadata,omitempty" json:"metadata,omitempty"`
-	Scheme   string                             `url:"scheme,omitempty" json:"scheme,omitempty"`
-	Verify   string                             `url:"verify,omitempty" json:"verify,omitempty"`
+	ConsentParameters *BillingRequestMandateRequestConsentParameters `url:"consent_parameters,omitempty" json:"consent_parameters,omitempty"`
+	Currency          string                                         `url:"currency,omitempty" json:"currency,omitempty"`
+	Links             *BillingRequestMandateRequestLinks             `url:"links,omitempty" json:"links,omitempty"`
+	Metadata          map[string]interface{}                         `url:"metadata,omitempty" json:"metadata,omitempty"`
+	Scheme            string                                         `url:"scheme,omitempty" json:"scheme,omitempty"`
+	Verify            string                                         `url:"verify,omitempty" json:"verify,omitempty"`
 }
 
 type BillingRequestPaymentRequestLinks struct {
@@ -965,9 +976,12 @@ type BillingRequestChooseCurrencyParams struct {
 
 // ChooseCurrency
 // This will allow for the updating of the currency and subsequently the scheme
-// if needed for a billing request
-// this will only be available for mandate only flows, it will not support
-// payments requests or plans
+// if
+// needed for a Billing Request. This will only be available for mandate only
+// flows
+// which do not have the lock_currency flag set to true on the Billing Request
+// Flow. It
+// will also not support any request which has a payments request.
 func (s *BillingRequestServiceImpl) ChooseCurrency(ctx context.Context, identity string, p BillingRequestChooseCurrencyParams, opts ...RequestOption) (*BillingRequest, error) {
 	uri, err := url.Parse(fmt.Sprintf(s.config.Endpoint()+"/billing_requests/%v/actions/choose_currency",
 		identity))
