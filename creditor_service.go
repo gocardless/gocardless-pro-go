435a5dbdbb009a08db49a8493bbb24f75302a3ec
Robot <robot@gocardless.com>
2023-05-10 11:42:52 +0000

    Changes generated by de4c948b91f245b471550142b17e2a6b8f0cc8ce

--- creditor_service.go
+++ creditor_service.go
@@ -86,7 +86,6 @@ type CreditorService interface {
 	All(ctx context.Context, p CreditorListParams, opts ...RequestOption) *CreditorListPagingIterator
 	Get(ctx context.Context, identity string, p CreditorGetParams, opts ...RequestOption) (*Creditor, error)
 	Update(ctx context.Context, identity string, p CreditorUpdateParams, opts ...RequestOption) (*Creditor, error)
-	ApplySchemeIdentifier(ctx context.Context, identity string, p CreditorApplySchemeIdentifierParams, opts ...RequestOption) (*Creditor, error)
 }
 
 // CreditorCreateParams parameters
@@ -572,124 +571,6 @@ func (s *CreditorServiceImpl) Update(ctx context.Context, identity string, p Cre
 	body = &buf
 
 	req, err := http.NewRequest("PUT", uri.String(), body)
-	if err != nil {
-		return nil, err
-	}
-	req = req.WithContext(ctx)
-	req.Header.Set("Authorization", "Bearer "+s.config.Token())
-	req.Header.Set("GoCardless-Version", "2015-07-06")
-	req.Header.Set("GoCardless-Client-Library", "gocardless-pro-go")
-	req.Header.Set("GoCardless-Client-Version", "3.1.0")
-	req.Header.Set("User-Agent", userAgent)
-	req.Header.Set("Content-Type", "application/json")
-	req.Header.Set("Idempotency-Key", o.idempotencyKey)
-
-	for key, value := range o.headers {
-		req.Header.Set(key, value)
-	}
-
-	client := s.config.Client()
-	if client == nil {
-		client = http.DefaultClient
-	}
-
-	var result struct {
-		Err      *APIError `json:"error"`
-		Creditor *Creditor `json:"creditors"`
-	}
-
-	err = try(o.retries, func() error {
-		res, err := client.Do(req)
-		if err != nil {
-			return err
-		}
-		defer res.Body.Close()
-
-		err = responseErr(res)
-		if err != nil {
-			return err
-		}
-
-		err = json.NewDecoder(res.Body).Decode(&result)
-		if err != nil {
-			return err
-		}
-
-		if result.Err != nil {
-			return result.Err
-		}
-
-		return nil
-	})
-	if err != nil {
-		return nil, err
-	}
-
-	if result.Creditor == nil {
-		return nil, errors.New("missing result")
-	}
-
-	return result.Creditor, nil
-}
-
-type CreditorApplySchemeIdentifierParamsLinks struct {
-	SchemeIdentifier string `url:"scheme_identifier,omitempty" json:"scheme_identifier,omitempty"`
-}
-
-// CreditorApplySchemeIdentifierParams parameters
-type CreditorApplySchemeIdentifierParams struct {
-	Links CreditorApplySchemeIdentifierParamsLinks `url:"links,omitempty" json:"links,omitempty"`
-}
-
-// ApplySchemeIdentifier
-// Applies a [scheme identifier](#core-endpoints-scheme-identifiers) to a
-// creditor.
-//
-// If the scheme identifier has a `pending` status, it will be applied
-// asynchronously
-// once it becomes `active`.
-//
-// If the creditor already has a scheme identifier for the scheme, it will be
-// replaced,
-// and any mandates attached to it transferred asynchronously. On Bacs and SEPA,
-// if
-// payments were about to be submitted, they will be delayed. To minimise this
-// delay, we
-// recommend that you apply the new scheme identifier after the daily payment
-// submission
-// time (4 PM Europe/London time).
-func (s *CreditorServiceImpl) ApplySchemeIdentifier(ctx context.Context, identity string, p CreditorApplySchemeIdentifierParams, opts ...RequestOption) (*Creditor, error) {
-	uri, err := url.Parse(fmt.Sprintf(s.config.Endpoint()+"/creditors/%v/actions/apply_scheme_identifier",
-		identity))
-	if err != nil {
-		return nil, err
-	}
-
-	o := &requestOptions{
-		retries: 3,
-	}
-	for _, opt := range opts {
-		err := opt(o)
-		if err != nil {
-			return nil, err
-		}
-	}
-	if o.idempotencyKey == "" {
-		o.idempotencyKey = NewIdempotencyKey()
-	}
-
-	var body io.Reader
-
-	var buf bytes.Buffer
-	err = json.NewEncoder(&buf).Encode(map[string]interface{}{
-		"data": p,
-	})
-	if err != nil {
-		return nil, err
-	}
-	body = &buf
-
-	req, err := http.NewRequest("POST", uri.String(), body)
 	if err != nil {
 		return nil, err
 	}
