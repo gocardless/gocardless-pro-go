3a6917fae2157196515d5cba6bbba0d9ccd2edb3
Robot <robot@gocardless.com>
2024-08-06 14:33:52 +0000

    Changes generated by 0ce4f0be685e1129b19a5cbd1884d65138a50780

--- event_service.go
+++ event_service.go
@@ -166,7 +166,7 @@ func (s *EventServiceImpl) List(ctx context.Context, p EventListParams, opts ...
 	req.Header.Set("Authorization", "Bearer "+s.config.Token())
 	req.Header.Set("GoCardless-Version", "2015-07-06")
 	req.Header.Set("GoCardless-Client-Library", "gocardless-pro-go")
-	req.Header.Set("GoCardless-Client-Version", "3.10.0")
+	req.Header.Set("GoCardless-Client-Version", "3.11.0")
 	req.Header.Set("User-Agent", userAgent)
 
 	for key, value := range o.headers {
@@ -275,7 +275,7 @@ func (c *EventListPagingIterator) Value(ctx context.Context) (*EventListResult, 
 	req.Header.Set("Authorization", "Bearer "+s.config.Token())
 	req.Header.Set("GoCardless-Version", "2015-07-06")
 	req.Header.Set("GoCardless-Client-Library", "gocardless-pro-go")
-	req.Header.Set("GoCardless-Client-Version", "3.10.0")
+	req.Header.Set("GoCardless-Client-Version", "3.11.0")
 	req.Header.Set("User-Agent", userAgent)
 
 	for key, value := range o.headers {
@@ -367,7 +367,7 @@ func (s *EventServiceImpl) Get(ctx context.Context, identity string, opts ...Req
 	req.Header.Set("Authorization", "Bearer "+s.config.Token())
 	req.Header.Set("GoCardless-Version", "2015-07-06")
 	req.Header.Set("GoCardless-Client-Library", "gocardless-pro-go")
-	req.Header.Set("GoCardless-Client-Version", "3.10.0")
+	req.Header.Set("GoCardless-Client-Version", "3.11.0")
 	req.Header.Set("User-Agent", userAgent)
 
 	for key, value := range o.headers {
